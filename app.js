3ab11b9a064bf1b0e844ea49c3daf91585cea193
KELLEN BUSBY <Kellen.Busby@LibertyMutual.com>
2017-01-04 12:22:57 -0500

    Updating to store session id and use that for apiai

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const Apiai = require('./model/Apiai');
 
 // Require routes
 const index = require('./routes/index');
 const event = require('./routes/event');
+const hook = require('./routes/hook');
 const pantryAnalyzer = require('./routes/pantryAnalyzer');
 
 // Load config
@@ -14,9 +17,44 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+const intentEventNames = ['dinner-time', 'item-will-expire-soon', 'new-item-purchased', 'notices-unhealthy-eating-habits', 'open-cabinet'];
+const genericEventNames = ['item-used'];
+let sessionId;
+
 // Declare routes
 app.use('/', index);
-app.use('/event', event);
+app.use('/event', (req, res) => {
+	let body = req.body,
+		eventName;
+
+	if(body) {
+		eventName = body.name;
+	} else {
+		return res.status(400).send('No event name on the request');
+	}
+
+	if(intentEventNames.includes(eventName)) {
+		let apiai = new Apiai(sessionId);
+		apiai.triggerEvent(eventName).then((result) => {
+			return res.status(200).send(result || 'event triggered');
+		}).catch(err => {
+			return res.status(500).send(err);
+		})
+	} else if (genericEventNames.includes(eventName)) {
+
+	} else {
+		return res.status(400).send(eventName + ' is not a valid event name.');
+	}
+});
+app.use('/hook', (req, res) => {
+	console.log('Hook route: Request for hook route');
+
+	let body = req.body;
+
+	sessionId = body.id;
+	console.log('successfully set sessionId');
+	res.send('sessionId set');
+});
 app.use('/pantryAnalyzer', pantryAnalyzer);
 
 // Start expiration watcher
@@ -28,4 +66,4 @@ app.listen(config.port, () => {
 	console.log(`Server listening on port ${config.port}`);
 });
 
-module.exports = expirationWatcher;
+module.exports = app;
--- routes/hook.js
+++ routes/hook.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const app = require('../app');
 
 // serves a very simplistic UI
 router.post('/', function(req, res) {
     console.log('Hook route: Request for hook route');
-    
-    
+
+    let body = req.body;
+
+    app.locals.id = body.id;
+    console.log('successfully set sessionId');
+    res.send('sessionId set');
 });
 
 module.exports = router;
